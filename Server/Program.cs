1850884978f1a88747d107a0908c590189dc8d68
Gabriel Rentschler <madiness444555666@gmail.com.br>
2020-11-03 08:12:12 -0300

    Unity Server V.1.0 - Simple TCP Connection

---
+++ Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Super Server";
+            Server.Start(25, 25565);
+
+            Console.ReadKey();
+        }
+    }
+}
